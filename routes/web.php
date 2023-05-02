cc2e454d8e461f3ccc09a7b894cc56495eb31ef8
lolzxc <guitarbeginner2@gmail.com>
2023-05-02 13:26:23 +0800

    added sign in and register route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'index')->name('index');
 Route::view('/about', 'about')->name('about');
 Route::view('/home', 'home')->name('home');
+Route::view('/signin', 'signin')->name('signin');
+Route::view('/register', 'register')->name('register');
